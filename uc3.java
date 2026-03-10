8a7dc6a6c2a024458d224d9a66dfc2211253c7e4
kishore kathir R <kishore19752008@gmail.com>
2026-03-10 20:46:23 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,18 @@
+/** 
+* @author kishorekathir 
+* @version 2.0 
+*/ 
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", " ***** ", " ***** ", " ****** ", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     *"));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*     *", "*     *", "****** ", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "*     *"));
+        System.out.println(String.join(" ", " ***** ", " ***** ", "*      ", " ***** "));
+
+    }
+}
